0fb002493d884418c5c28a993d36bea7ea810188
valeponick <vale.ponick@gmail.com>
2026-06-25 05:20:31 +0700

    Initial Commit

---
+++ CodeLogicMind/CodeLogicMindApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeLogicMindApp.swift
+//  CodeLogicMind
+//
+//  Created by Валерия Пономарева on 25.06.2026.
+//
+
+import SwiftUI
+
+@main
+struct CodeLogicMindApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
